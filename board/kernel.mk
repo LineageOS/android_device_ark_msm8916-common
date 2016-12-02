3aa19089823647fe98eb4d864ff2b44d402c15fe
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-12-02 14:06:41 +0600

    msm8916-common: Changes for kernel build

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,17 +1,12 @@
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
-
 ENABLE_CPUSETS := true
-
 TARGET_KERNEL_SOURCE := kernel/ark/msm8916
-ifneq ($(FORCE_32_BIT),true)
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_USES_UNCOMPRESSED_KERNEL := true
-else
 TARGET_KERNEL_ARCH := arm
-endif
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_USES_UNCOMPRESSED_KERNEL := true
